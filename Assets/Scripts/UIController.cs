70eda9549abce6491c42f61ab3990b45ba3ca4c4
EncostoCWB <mic.gi.rodrigues@hotmail.com>
2024-04-10 23:38:19 -0300

    Botoes setados - o de voltar
    eh isso

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIController : MonoBehaviour
 {
@@ -10,17 +11,21 @@ public class UIController : MonoBehaviour
 
     [Header("TELAS DO CADERNO")] 
     public GameObject telaInicialDoCaderno;
+    
     [Header("TELAS DE RESPOSTA UNITARIA")]
     public GameObject telaRespostUnit;
     public GameObject telaRespArt;
     public GameObject telaRespSumula;
+    
     [Header("TELAS DE PECA PROCESSUAL")]
     public GameObject telaPeticao;
+    
     [Header("TELAS DE PETICAO ESPECIAL")]
     public GameObject telaPeticaoEspecial;
     public GameObject telaFundEspecial;
     public GameObject telaRespArtPEspecial;
     public GameObject telaRespSumulaPEspecial;
+    
     [Header("TELAS DE PETICAO ORDINARIA")]
     public GameObject telaPeticaoOrdinaria;
     public GameObject TelaFundOrdinaria;
@@ -38,7 +43,7 @@ public class UIController : MonoBehaviour
 
     public void Start()
     {
-        telasInicio[contadorTelaInicio].SetActive(true);
+        telasInicio[contadorTelaInicio].SetActive(true); 
     }
 
     public void AvancarTelaInicio()
@@ -60,53 +65,70 @@ public class UIController : MonoBehaviour
     {
         switch (x)
         {
-            case 1:
+            case 0:
                 telaJogo.SetActive(false);
                 telaInicialDoCaderno.SetActive(true);
                 break;
-            case 2:
+            case 1:
                 telaJogo.SetActive(false);
                 telaComputador.SetActive(true);
                 break;
-            case 3:
+            case 2:
                 telaInicialDoCaderno.SetActive(false);
                 telaRespostUnit.SetActive(true);
+                break;
+            case 3:
+                telaInicialDoCaderno.SetActive(false);
+                telaPeticao.SetActive(true);
                 break;
             case 4:
                 telaRespostUnit.SetActive(false);
                 telaRespArt.SetActive(true);
+                break;
+            case 5:
+                telaRespostUnit.SetActive(false);
+                telaRespSumula.SetActive(true);
+                break;
+            case 6:
+                telaPeticao.SetActive(false);
+                telaPeticaoEspecial.SetActive(true);
+                break;
+            case 7:
+                telaPeticao.SetActive(false);
+                telaPeticaoOrdinaria.SetActive(true);
+                break;
+            case 8:
+                telaPeticaoEspecial.SetActive(false);
+                telaFundEspecial.SetActive(true);
+                break;
+            case 9:
+                telaFundEspecial.SetActive(false);
+                telaRespArtPEspecial.SetActive(true);
+                break;
+            case 10:
+                telaFundEspecial.SetActive(false);
+                telaRespSumulaPEspecial.SetActive(true);
+                break;
+            case 11:
+                telaPeticaoOrdinaria.SetActive(false);
+                TelaFundOrdinaria.SetActive(true);
+                break;
+            case 12:
+                TelaFundOrdinaria.SetActive(false);
+                TelaRespArtPOrdinaria.SetActive(true);
+                break;
+            case 13:
+                TelaFundOrdinaria.SetActive(false);
+                TelaRespSumulaPOrdinaria.SetActive(true);
                 break;
         }
     }
 
     public void VoltarTelas(int x)
     {
-        switch (x)
-        {
-            case 0:
-                telaInicialDoCaderno.SetActive(false);
-                telaJogo.SetActive(true);
-                break;
-            case 1:
-                telaComputador.SetActive(false);
-                telaJogo.SetActive(true);
-                break;
-            case 2:
-                telaRespostUnit.SetActive(false);
-                telaJogo.SetActive(true);
-                break;
-            case 3:
-                telaRespostUnit.SetActive(false);
-                telaInicialDoCaderno.SetActive(true);
-                break;
-            case 4:
-                telaRespArt.SetActive(false);
-                telaJogo.SetActive(true);
-                break;
-            case 5:
-                telaRespArt.SetActive(false);
-                telaRespostUnit.SetActive(true);
-                break;
-        }
+        //switch (x)
+        //{
+            
+        //}
     }
 }
