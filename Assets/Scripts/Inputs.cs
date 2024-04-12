3770939fa3ad2a7042b3c3555913fec195a09de5
VirtuK <vitorkuiava.vc@gmail.com>
2024-04-11 21:38:58 -0300

    Questões A e B do primeiro caso, com a A já testando se foi respondida

--- Assets/Scripts/Inputs.cs
+++ Assets/Scripts/Inputs.cs
@@ -10,6 +10,25 @@ public class Inputs : MonoBehaviour
     public SceneControl SC;
     public TMP_InputField input;
     public TMP_Dropdown dropdown;
+
+    public string fundamentacao;
+    public string artigo;
     // Start is called before the first frame update
    
+
+    public void inserirArtigo(string art) //Insere o artigo digitado numa variavel 
+    {
+        artigo = art;
+    }
+
+    public void inserirFundamentacao(string fund) //Insere a fundamentacao digitada numa variavel
+    {
+        fundamentacao = fund;
+    }
+
+
+    public void FinalizarResposta() //Transforma tudo que foi digitado pelo candidato na resposta oficial da questao e manda para o SceneControl
+    {
+        SC.resposta1 = artigo +  "," + fundamentacao;
+    }
 }
--- Assets/Scripts/SceneControl.cs
+++ Assets/Scripts/SceneControl.cs
@@ -7,10 +7,33 @@ using UnityEngine;
 public class SceneControl : MonoBehaviour
 {
     public DataHandler data;
-    public TMP_Text questao;
-    public void SetQuestion(int x)
+
+    public TMP_Text texto_caso;
+    public TMP_Text questaoA;
+    public TMP_Text questaoB;
+
+    public string resposta1 = "";
+
+
+    private void Start() //Desativa a questao B e posteriormente vai desativar todas alem da A quando o jogo começa
     {
-        questao.text = data.rawdata[x][0];
+        questaoB.gameObject.SetActive(false);
+    }
+    public void SetQuestion(int x) //Seta o texto de tudo da tela do computador, conforme o sheets
+    {
+        texto_caso.text = data.rawdata[x][0];
+        questaoA.text = "a)" + " " + data.rawdata[x][1];
+        questaoB.text = "b)" + " " + data.rawdata[x][2];
+    }
+
+    private void Update()
+    {
+        
+        if(resposta1 != "") //Verifica se a resposta oficial da questao foi atualizada e se sim risca a questao a e ativa a questao b
+        {
+            questaoA.fontStyle = FontStyles.Strikethrough;
+            questaoB.gameObject.SetActive(true);
+        }
     }
 }
     
