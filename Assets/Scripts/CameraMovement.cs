bae38a328c352f42c65b212f9387ef21375fcf3a
VirtuK <vitorkuiava.vc@gmail.com>
2024-05-03 00:20:16 -0300

    Fiz o movimento inicial da camera e ajustei as telas para encaixarem no pc

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    float cameraInitialZ;
+    float cameraActualZ;
+    public Transform Camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraInitialZ = Camera.position.z;
+        cameraActualZ = Camera.transform.position.z;
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Camera.position.z < 14)
+        {
+            cameraActualZ += 0.05f;
+            Camera.position = new Vector3(Camera.position.x, Camera.position.y, cameraActualZ);
+            print(cameraActualZ);
+           
+        }
+        else
+        {
+            this.GetComponent<CameraMovement>().enabled = false;
+        }
+    }
+}
